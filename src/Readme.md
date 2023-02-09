f9adb3fee088e40a351d143a973bd1775b87ce5d
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2023-02-09 13:33:19 +0100

    :D

---
+++ src/Readme.md
@@ -0,0 +1,3 @@
+# Loggbok
+## 9-2-2023
+Jag har börjat skapa strukturen för mvc. Jag har gjort ett enkelt Gui och börjat på server Modellen.
--- src/server/model/Server.java
+++ src/server/model/Server.java
@@ -16,7 +16,7 @@ public class Server {
 
     public void acceptClients()
     {
-        
+
     }
 
     public static void main(String[] args) {
